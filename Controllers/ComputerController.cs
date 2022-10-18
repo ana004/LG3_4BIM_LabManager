79493b9eaeeed2f5ef9d414215082475dcfb1e4d
ana004 <paulaana08002019@gmail.com>
2022-10-18 15:28:23 -0300

    Adicionado o projeto MvcLabManager

---
+++ Controllers/ComputerController.cs
@@ -0,0 +1,29 @@
+using Microsoft.AspNetCore.Mvc;
+using MvcLabManager.Models;
+
+namespace MvcLabManager.Controllers;
+
+public class ComputerController : Controller
+{
+    // underline pode ser usada se atributo é private
+    private readonly LabManagerContext _context;
+
+    public ComputerController(LabManagerContext context) 
+    {
+        _context = context;
+    }
+
+    public IActionResult Index() => View(_context.Computers);
+
+    public IActionResult Show(int id)
+    {
+        Computer computer = _context.Computers.Find(id);
+
+        if(computer == null)
+        {
+            return NotFound(); // HttpNotFound()
+        }
+
+        return View(computer);
+    }
+}
---
+++ Models/LabManagerContext.cs
@@ -0,0 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MvcLabManager.Models;
+
+public class LabManagerContext : DbContext
+{
+    //DbSet para Computer, Lab, etc
+
+    public DbSet<Computer> Computers { get; set; }
+
+    public LabManagerContext(DbContextOptions<LabManagerContext> options) : base(options)
+    //base é construtor da classe DbContextOptions. Igual a classe super do Java
+    {
+
+    }
+}
