211c35c14d9fe0167b1bf3b361895371c99c7971
ana004 <paulaana08002019@gmail.com>
2022-10-24 10:05:09 -0300

    Adicionada a funcionalidade Create para Labs

--- Controllers/LabController.cs
+++ Controllers/LabController.cs
@@ -14,6 +14,28 @@ public class LabController : Controller
 
     public IActionResult Index() => View(_context.Labs);
 
+    public IActionResult Create()
+    {
+        return View();
+    }
+
+    [HttpPost]
+    public IActionResult Create([FromForm] int number, [FromForm] string name, [FromForm] string block)
+    {
+        ErrorExistsByLabNumber(number);
+        ErrorExistsByLabName(name);
+
+        List<Lab> labs = _context.Labs.ToList();
+        int id = labs.Last().Id + 1; 
+        
+        Lab lab = new Lab(id, number, name, block);
+
+        _context.Labs.Add(lab);
+        _context.SaveChanges();
+
+        return RedirectToAction(nameof(Index));
+    }
+
     public IActionResult Show(int id)
     {
         Lab lab = _context.Labs.Find(id);
@@ -24,5 +46,17 @@ public class LabController : Controller
         }
 
         return View(lab);
+    }
+
+    private void ErrorExistsByLabNumber(int number) 
+    {    
+        if(_context.Labs.Any(e => e.Number.Equals(number)))
+            throw new Exception($"Já existe um laboratório com número {number}");
+    }
+
+    private void ErrorExistsByLabName(string name) 
+    {    
+        if(_context.Labs.Any(e => e.Name.Equals(name)))
+            throw new Exception($"O laboratório {name} já existe");
     }
 }
