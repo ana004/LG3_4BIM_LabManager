141ce577280e4e624391af40574471ca36441322
ana004 <paulaana08002019@gmail.com>
2022-10-24 08:37:14 -0300

    Adicionada a entidade Labs com a funcionalidade Index

--- Models/LabManagerContext.cs
+++ Models/LabManagerContext.cs
@@ -8,6 +8,8 @@ public class LabManagerContext : DbContext
 
     public DbSet<Computer> Computers { get; set; }
 
+    public DbSet<Lab> Labs { get; set; }
+
     public LabManagerContext(DbContextOptions<LabManagerContext> options) : base(options)
     //base é construtor da classe DbContextOptions. Igual a classe super do Java
     {
